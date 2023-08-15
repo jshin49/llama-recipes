010aabbf4a4f4ad9e0cea36d0c6b617b55491ccc
SeungoneKim <louisdebroglie@kaist.ac.kr>
2023-08-15 13:31:50 +0000

    add pip req

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+# pip
+pip  install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
+
+pip install accelerate appdirs loralib bitsandbytes==0.39.1 black datasets fire transformers>=4.31.0 sentencepiece py7zr scipy optimum peft
+
 # 13B full fine-tuning
 CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --nnodes 1 --nproc_per_node 4  llama_finetuning.py --enable_fsdp --pure_bf16 --model_name meta-llama/Llama-2-13b-chat-hf --hf_cache_dir /home/seungone/hf_cache --batch_size_training 4 --micro_batch_size 4 --dist_checkpoint_root_folder model_checkpoints --dist_checkpoint_folder fine-tuned --use_fast_kernel
 
